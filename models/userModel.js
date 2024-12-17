f6be8cbf0771efd5bd7ed78f68db9972203de3c2
Chris <chrisdietrich3666@gmail.com>
2024-12-16 19:12:33 -0500

    added googlesso

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+	familyName: { type: String, required: false },
+	givenName: { type: String, required: false },
+	googleId: { type: String, required: false },
+	photo: { type: String, required: false },
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: false },
